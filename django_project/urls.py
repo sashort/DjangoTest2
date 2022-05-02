c8905b3ded59fcc225044458327a6058fda4df27
Andy Short <essayshort@gmail.com>
2022-05-02 12:09:41 -0400

    Older url() test

--- django_project/urls.py
+++ django_project/urls.py
@@ -15,12 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from django.conf.urls import include#, url
+from django.conf.urls import include, url
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('accounts.urls')),
-	path('accounts/', include('django.contrib.auth.urls')),
-	path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    #path('admin/', admin.site.urls),
+    #path('accounts/', include('accounts.urls')),
+	#path('accounts/', include('django.contrib.auth.urls')),
+	#path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    url(r'^admin/', admin.site.urls),
+    url(r'^accounts/', include('accounts.urls')),
+	url(r'^accounts/', include('django.contrib.auth.urls')),
+	url('', TemplateView.as_view(template_name='home.html'), name='home'),
+	
 ]
