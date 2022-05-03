43da048395118690c9418d5448418e778ba19946
Andy Short <essayshort@gmail.com>
2022-05-03 15:26:07 -0400

    manually created TaskItem migration

--- tasks/views.py
+++ tasks/views.py
@@ -13,8 +13,7 @@ def task_list_view(request):
         if data.get("action") != None: #data.get("action") = "add"
             TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
         else: #deleting item
-            print("Attempting to remove: " + data.get("delete_index"))
-            TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int(data.get("delete_index"))))
+            TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int(data.get("delete_index")))).delete()
     context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk)}
     return render(request, "task_list.html",context)
 	
