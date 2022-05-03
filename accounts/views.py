20610d29a2769e17f4e056bc1148774c186cb1e1
Andy Short <essayshort@gmail.com>
2022-05-03 09:56:29 -0400

    Fixed yet another indentation error. Why am I still using Notepad++?

--- accounts/views.py
+++ accounts/views.py
@@ -15,9 +15,9 @@ def task_list_view(request):
     if request.method == "POST":
         data = request.POST
         if action != None: #data.get("action") = "add"
-		    TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
+            TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
         else: #deleting item
             TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int("delete_index")))
-		return render(request, "task_list.html",TaskItem.objects.filter(user_pk=request.user.pk))
+        return render(request, "task_list.html",TaskItem.objects.filter(user_pk=request.user.pk))
     context = {'task_list':tasks}
     return render(request, "task_list.html",context)
