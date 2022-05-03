36da515bb1c9cf672001e248fbe4ba5e250410c1
Andy Short <essayshort@gmail.com>
2022-05-03 10:32:04 -0400

    Changed task_list_view context

--- accounts/views.py
+++ accounts/views.py
@@ -19,6 +19,5 @@ def task_list_view(request):
             TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
         else: #deleting item
             TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int("delete_index")))
-        return render(request, "task_list.html",TaskItem.objects.filter(user_pk=request.user.pk))
-    context = {'task_list':tasks}
+    context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk)}
     return render(request, "task_list.html",context)
