ce838447067d3b7870792d387b6ae84dda42d759
Andy Short <essayshort@gmail.com>
2022-05-03 15:46:52 -0400

    manually created TaskItem migration

--- tasks/views.py
+++ tasks/views.py
@@ -8,13 +8,15 @@ from django.shortcuts import render
 
 def task_list_view(request):
     tasks = TaskItem.objects.filter(user_pk=request.user.pk)
+	delete_index = "N/A"
     if request.method == "POST":
         data = request.POST
         if data.get("action") != None: #data.get("action") = "add"
             TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
         else: #deleting item
             TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int(data.get("delete_index")))).delete()
-    context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk)}
+            delete_index = int(data.get("delete_index")
+    context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk), 'delete_index': delete_index}
     return render(request, "task_list.html",context)
 	
 	
