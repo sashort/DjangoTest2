ebdfe76cb3e44797dc456e0e7b0bb27e63fa29d6
Andy Short <essayshort@gmail.com>
2022-05-03 14:07:55 -0400

    attempting to fix unknown error

--- tasks/views.py
+++ tasks/views.py
@@ -10,7 +10,7 @@ def task_list_view(request):
     tasks = TaskItem.objects.filter(user_pk=request.user.pk)
     if request.method == "POST":
         data = request.POST
-        if action != None: #data.get("action") = "add"
+        if data.get("action") != None: #data.get("action") = "add"
             TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
         else: #deleting item
             TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int("delete_index")))
