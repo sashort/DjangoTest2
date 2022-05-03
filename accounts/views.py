4647effd9668b8115bab9fe6f83022fde28bb958
Andy Short <essayshort@gmail.com>
2022-05-03 10:01:11 -0400

    'from [.]models import TaskItem'

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,7 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from models import TaskItem
+from .models import TaskItem
 
 
 class SignUpView(generic.CreateView):
