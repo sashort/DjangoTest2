f60c95ea0a8d7df3ff248e126f41ee219a7f3b80
Andy Short <essayshort@gmail.com>
2022-05-03 12:54:49 -0400

    wow

---
+++ .venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\essay\OneDrive\Desktop\django_auth\.venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,4 +6,5 @@ from .views import SignUpView, task_list_view
 
 urlpatterns = [
     url(r"^signup/", SignUpView.as_view(), name="signup"),
+    url(r"^tasks/", task_list_view, name="task list"),
 ]
