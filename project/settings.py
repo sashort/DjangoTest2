0e3f318c4333c48e00c4d625c333daf6349515fd
Andy Short <essayshort@gmail.com>
2022-05-03 13:19:06 -0400

    added tasks to installed apps

--- project/settings.py
+++ project/settings.py
@@ -42,8 +42,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'debug_toolbar',
+    'debug_toolbar',
     'accounts',
+    'tasks',
 ]
 
 MIDDLEWARE = [
