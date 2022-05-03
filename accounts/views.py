c229be24bd82e679a1604fa56a447f000619198e
Andy Short <essayshort@gmail.com>
2022-05-03 13:11:51 -0400

    rremoved erroneous import

--- accounts/views.py
+++ accounts/views.py
@@ -2,7 +2,6 @@
 from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views import generic
-from .models import TaskItem
 from django.shortcuts import render
 
 
