e7b6c2d5fa1304f02a0d265e546e5127aaa37976
Andy Short <essayshort@gmail.com>
2022-05-04 16:23:31 -0400

    Final. set debug=FALSE

--- tasks/views.py
+++ tasks/views.py
@@ -7,16 +7,16 @@ from django.shortcuts import render
 
 
 def task_list_view(request):
-    fade_in = True
     tasks = TaskItem.objects.filter(user_pk=request.user.pk)
     if request.method == "POST":
-        fade_in = False
         data = request.POST
         if data.get("action") == "add": #data.get("action") = "add"
             TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
         else: #deleting item
             TaskItem.objects.get(id=int(data.get("delete_index"))).delete()
-    context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk), 'fade_in': fade_in}
+        context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk)}
+    else:
+        context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk), 'fade_in': True}
     return render(request, "task_list.html",context)    
 	
 	
