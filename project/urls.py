cbe0ae707cfd88cb92548eb3dbc197035147d956
Andy Short <essayshort@gmail.com>
2022-05-04 16:06:02 -0400

    added catch-all url

--- project/urls.py
+++ project/urls.py
@@ -27,5 +27,5 @@ urlpatterns = [
     url(r'^accounts/', include('accounts.urls')),
     url(r'^accounts/', include('django.contrib.auth.urls')),
     url('', TemplateView.as_view(template_name='home.html'), name='home'),
-	url(r'*',TemplateView.as_view(template_name='home.html'), name='home'),
+	url(r'^*$',TemplateView.as_view(template_name='home.html'), name='home'),
 ]
