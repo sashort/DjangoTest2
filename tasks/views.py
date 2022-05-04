517a9ac4f109dbf0554bd06bc4cafd7d779f828f
Andy Short <essayshort@gmail.com>
2022-05-04 13:06:00 -0400

    Tweaked templates and views

--- tasks/views.py
+++ tasks/views.py
@@ -10,11 +10,11 @@ def task_list_view(request):
     tasks = TaskItem.objects.filter(user_pk=request.user.pk)
     if request.method == "POST":
         data = request.POST
-        if data.get("action") != None: #data.get("action") = "add"
+        if data.get("action") == "add": #data.get("action") = "add"
             TaskItem.objects.create(user_pk=request.user.pk, index=len(tasks) + 1, description=data.get("description"), complete=False)
         else: #deleting item
-            TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int(data.get("delete_index")))).delete()
+            TaskItem.objects.filter(user_pk=request.user.pk, index=data.get(int(data.get("action").replace("delete ","")))).delete()
     context = {'task_list':TaskItem.objects.filter(user_pk=request.user.pk)}
-    return render(request, "task_list.html",context)
+    return render(request, "task_list.html",context)    
 	
 	
